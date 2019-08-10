c812aac7a20c72aa99b716be10aee3edb03fa19c
John Crane <john.crane@gmail.com>
2019-08-09 20:30:15 -0700

    Moved all code into Main.elm

---
+++ src/Main.elm
@@ -0,0 +1,114 @@
+module Main exposing (Model, Msg(..), init, main, update, view, viewStuff, viewWelcome)
+
+import Browser exposing (Document, document)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Maybe exposing (Maybe)
+import Random exposing (Seed, initialSeed, int, maxInt, minInt, step)
+import String exposing (fromInt)
+import Task exposing (Task, perform)
+import Time exposing (Posix, now, posixToMillis)
+
+
+type alias Model =
+    { randomSeed : Maybe Seed
+    , startTime : Maybe Posix
+    }
+
+
+type Msg
+    = CloseWelcomeScreen
+    | StartApp Posix
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subs
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model Nothing Nothing, Cmd.none )
+
+
+subs : Model -> Sub Msg
+subs model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        CloseWelcomeScreen ->
+            ( model, Task.perform StartApp Time.now )
+
+        StartApp time ->
+            ( { model
+                | startTime = Just time
+                , randomSeed = Just (initialSeed (posixToMillis time))
+              }
+            , Cmd.none
+            )
+
+
+view : Model -> Document Msg
+view model =
+    let
+        b =
+            case model.startTime of
+                Nothing ->
+                    viewWelcome model
+
+                Just t ->
+                    viewStuff model
+    in
+    { title = "Elm App Template"
+    , body = [ b ]
+    }
+
+
+viewStuff : Model -> Html Msg
+viewStuff model =
+    let
+        seed =
+            case model.randomSeed of
+                Nothing ->
+                    "unknown"
+
+                Just s ->
+                    Random.step (int minInt maxInt) s
+                        |> Tuple.first
+                        |> fromInt
+
+        time =
+            case model.startTime of
+                Nothing ->
+                    "unknown"
+
+                Just t ->
+                    t |> posixToMillis |> fromInt
+    in
+    div []
+        [ p []
+            [ text "Start time is "
+            , text time
+            ]
+        , p []
+            [ text "Your psuedo random number is  "
+            , text seed
+            ]
+        ]
+
+
+viewWelcome : Model -> Html Msg
+viewWelcome model =
+    div []
+        [ p [] [ h1 [] [ text "Welcome!" ] ]
+        , button [ onClick CloseWelcomeScreen ] [ text "Ok" ]
+        ]
--- tests/AppTests.elm
+++ tests/AppTests.elm
@@ -3,15 +3,13 @@ module AppTests exposing (suite)
 import Expect exposing (true)
 import Fuzz exposing (int)
 import Html.Events exposing (..)
-import Model exposing (..)
+import Main exposing (..)
 import Test exposing (..)
 import Test.Html.Event as E
 import Test.Html.Query as Q
 import Test.Html.Selector as S
 import Time exposing (millisToPosix)
 import Tuple exposing (first)
-import Update
-import View
 
 
 suite : Test
@@ -19,26 +17,13 @@ suite =
     describe "Views"
         [ test "Welcome view has an OK button" <|
             \() ->
-                View.viewWelcome (Update.init () |> first)
+                viewWelcome (init () |> first)
                     |> Q.fromHtml
                     |> Q.find [ S.tag "button" ]
-                    |> Q.has [ S.text "OK" ]
+                    |> Q.has [ S.text "Ok" ]
         , test "OK button returns a CloseWelcomeScreen" <|
             \() ->
-                View.viewWelcome (Update.init () |> first)
-                    |> Q.fromHtml
-                    |> Q.find [ S.tag "button" ]
-                    |> E.simulate E.click
-                    |> E.expect CloseWelcomeScreen
-        , test "Main view has an restart button" <|
-            \() ->
-                View.viewStuff (Update.init () |> first)
-                    |> Q.fromHtml
-                    |> Q.find [ S.tag "button" ]
-                    |> Q.has [ S.text "Restart" ]
-        , test "Restart button returns a CloseWelcomeScreen" <|
-            \() ->
-                View.viewStuff (Update.init () |> first)
+                viewWelcome (init () |> first)
                     |> Q.fromHtml
                     |> Q.find [ S.tag "button" ]
                     |> E.simulate E.click
@@ -47,9 +32,9 @@ suite =
             \r1 ->
                 let
                     model =
-                        Update.init () |> first
+                        init () |> first
                 in
-                Update.update (StartApp (Time.millisToPosix r1)) model
+                update (StartApp (Time.millisToPosix r1)) model
                     |> first
                     |> .randomSeed
                     |> Expect.notEqual model.randomSeed
